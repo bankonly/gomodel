3250285af6feb4a12869c043205ccbde791a4776
bankonly <for21bn01@gmail.com>
2021-11-15 21:43:42 +0700

    v0.1.1

--- user.go
+++ user.go
@@ -1,9 +1,13 @@
-package gomodel
+package main
 
 type LoginSchema struct {
 	Username string `json:"username"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+func main() {
+
 }
 
 // Commit

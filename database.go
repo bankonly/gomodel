f5847eafdfedc08bb355134ecee8ab65b247de63
bankonly <for21bn01@gmail.com>
2021-11-15 23:23:34 +0700

    update

---
+++ database.go
@@ -0,0 +1,26 @@
+package gomodel
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func Mongo(uri string) (*mongo.Client, context.Context, context.CancelFunc) {
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client, ctx, cancel
+}

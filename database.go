652bdb2fc945fd76874f703d07d5691c0ef44aa3
bankonly <for21bn01@gmail.com>
2021-11-17 00:03:04 +0700

    update

--- database.go
+++ database.go
@@ -2,31 +2,37 @@ package gomodel
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
-	"time"
+	"sync"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-func MongoInstance() (*mongo.Database, *mongo.Client, context.Context) {
-	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("DATABASE_URI")))
-	if err != nil {
-		log.Fatal(err)
-	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = client.Ping(ctx, readpref.Primary())
-	if err != nil {
-		log.Fatal(err)
-	}
+var client *mongo.Client
+var instance *mongo.Database
+var ctx context.Context
+var mongoOnce sync.Once
 
-	instance := client.Database(os.Getenv("DATABASE_NAME"))
+func MongoInstance() (*mongo.Database, *mongo.Client, context.Context) {
+	ctx := context.TODO()
+	mongoOnce.Do(func() {
+		fmt.Println("CONNECTED AGAIN")
+		clientOption := options.Client().ApplyURI(os.Getenv("DATABASE_URI"))
+		client, err := mongo.Connect(ctx, clientOption)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = client.Ping(ctx, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		instance = client.Database(os.Getenv("DATABASE_NAME"))
+	})
 
 	return instance, client, ctx
 }

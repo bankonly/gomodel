341d4ffe512c46e3c4cb9f8cff93f92dbc3fa426
bankonly <for21bn01@gmail.com>
2021-11-16 22:05:20 +0700

    update

--- user.go
+++ user.go
@@ -7,13 +7,13 @@ import (
 )
 
 type User struct {
-	ID        primitive.ObjectID `json:"_id" binding:"required"`
-	Username  string             `json:"username,required" binding:"required"`
-	Email     string             `json:"email,required" binding:"required"`
-	Password  string             `json:"password,required" binding:"required"`
-	CreatedAt time.Time          `json:"createdAt,required" binding:"required"`
-	UpdatedAt time.Time          `json:"updatedAt,required" binding:"required"`
-	DeletedAt time.Time          `json:"deletedAt,omitempty"`
+	ID        primitive.ObjectID `bson:"_id" binding:"required"`
+	Username  string             `bson:"username,required" binding:"required"`
+	Email     string             `bson:"email,required" binding:"required"`
+	Password  string             `bson:"password,required" binding:"required"`
+	CreatedAt time.Time          `bson:"createdAt,required" binding:"required"`
+	UpdatedAt time.Time          `bson:"updatedAt,required" binding:"required"`
+	DeletedAt time.Time          `bson:"deletedAt,omitempty"`
 }
 
 func UserModel(u *User) *User {

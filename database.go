616074ccfabb55a74dbc7461298f1c4dd8a89718
bankonly <for21bn01@gmail.com>
2021-11-16 14:48:39 +0700

    update

--- database.go
+++ database.go
@@ -10,30 +10,19 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func MongoInstance() (*mongo.Client, context.Context, context.CancelFunc) {
+func MongoInstance() (*mongo.Database, *mongo.Client, context.Context, context.CancelFunc) {
 	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("DATABASE_URI")))
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	instance := client.Database(os.Getenv("DATABASE_NAME"))
 
 	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return client, ctx, cancel
-}
-
-func MongoWithDBName(database string) (*mongo.Client, context.Context, context.CancelFunc) {
-	client, ctx, cancel := MongoInstance()
-	client.Database(database)
-	return client, ctx, cancel
-}
-
-func Mongo() (*mongo.Database, *mongo.Client, context.Context, context.CancelFunc) {
-	client, ctx, cancel := MongoInstance()
-	instance := client.Database(os.Getenv("DATABASE_NAME"))
 	return instance, client, ctx, cancel
 }

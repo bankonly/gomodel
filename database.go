92e4d2a06b5509393cd9c58f246ba791507fd04f
bankonly <for21bn01@gmail.com>
2021-11-17 00:07:18 +0700

    update

--- database.go
+++ database.go
@@ -9,7 +9,6 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/options/readpref"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 

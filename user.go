77722dffb4ed88cc7a3c254b53caa9fc08b60553
bankonly <for21bn01@gmail.com>
2021-11-16 21:54:20 +0700

    update

--- user.go
+++ user.go
@@ -8,11 +8,11 @@ import (
 
 type User struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty"`
-	Username  string             `bson:"username,omitempty"`
-	Email     string             `bson:"email,omitempty"`
-	Password  string             `bson:"password,omitempty"`
-	CreatedAt time.Time          `bson:"createdAt,omitempty"`
-	UpdatedAt time.Time          `bson:"updatedAt,omitempty"`
+	Username  string             `bson:"username,required"`
+	Email     string             `bson:"email,required"`
+	Password  string             `bson:"password,required"`
+	CreatedAt time.Time          `bson:"createdAt,required"`
+	UpdatedAt time.Time          `bson:"updatedAt,required"`
 	DeletedAt time.Time          `bson:"deletedAt,omitempty"`
 }
 

c93792fa8deb85b1705b3bdb126837825dc12232
bankonly <for21bn01@gmail.com>
2021-11-16 20:50:25 +0700

    update

--- user.go
+++ user.go
@@ -1,9 +1,25 @@
 package gomodel
 
+import "time"
+
 type User struct {
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	DeletedAt bool      `json:"deletedAt"`
+}
+
+func UserModel(username, email, password string) *User {
+	u := User{}
+	u.Username = username
+	u.Email = email
+	u.Password = password
+	u.CreatedAt = time.Now().UTC()
+	u.UpdatedAt = time.Now().UTC()
+	u.DeletedAt = false
+	return &u
 }
 
 // Update field

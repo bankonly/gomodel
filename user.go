b4b3d8f26986cdfc6e0dd2cc86a1b26e34f504a8
bankonly <for21bn01@gmail.com>
2021-11-16 15:05:31 +0700

    update

--- user.go
+++ user.go
@@ -1,12 +1,9 @@
 package gomodel
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 type User struct {
-	ID       primitive.ObjectID `json:"id"`
-	Username string             `json:"username"`
-	Email    string             `json:"email"`
-	Password string             `json:"password"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 // Update field
